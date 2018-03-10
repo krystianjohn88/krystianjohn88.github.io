36300f33b7bc7a2117cac015b0f7c0553598ad07
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-10 16:09:39 +0100

    js

---
+++ warszataty/javascript/js/komentarze.js
@@ -0,0 +1,9 @@
+// tak wyglada komentarz liniowy
+/*
+tutaj jest komentarz blokowy 
+z kilku linii sie skladajacy
+
+// a tu nie wiem po co zagniezdze sobie liniowy komentarz
+
+*/
+
---
+++ warszataty/javascript/js/zmeinne.js
@@ -0,0 +1,13 @@
+var imie;
+var nazwisko = "John";
+var wiek = 32;
+
+console.log(imie);
+
+imie ="Krystian"
+
+console.log(imie);
+
+//łaczenie tekstu
+var laczenieTekstu = "Moje imie to: " + imie + "a nazwisko to: " + nazwisko;
+console.log(laczenieTekstu)
