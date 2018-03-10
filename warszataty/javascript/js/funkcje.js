0ca28b3b6af74a2e473829e910100341b57032b3
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-10 16:38:47 +0100

    funkcje

---
+++ warszataty/javascript/js/funkcje.js
@@ -0,0 +1,15 @@
+// funkcja bez parametrow
+function wyswietlWKonsoli() {
+    console.log("Krystian John");
+}
+wyswietlWKonsoli();
+
+// funkcja z parametrami
+
+function wyswietlWKonsoliParam( imie, nazwisko) {
+    console.log( imie + " " + nazwisko );
+}
+
+wyswietlWKonsoliParam( "Adam", "Nowacki");
+wyswietlWKonsoliParam("Krystian", "John");
+wyswietlWKonsoliParam("Kinga", "Korycka");
