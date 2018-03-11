daf33a2b5f92d540cbb907da1117ee28d30f2dac
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-11 14:01:38 +0100

    math

---
+++ warszataty/javascript/js/math.js
@@ -0,0 +1,11 @@
+// Włąściwości obietku Math
+
+console.log( Math.E );
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+// Metody obietku Math
+
+console.log(Math.cos(30));
+console.log(Math.random());
+console.log(Math.pow(2,2));
