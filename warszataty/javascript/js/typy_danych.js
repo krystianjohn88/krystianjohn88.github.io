4873a9991c3ec039e0865b4233459a4b0a91da1f
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-11 10:33:46 +0100

    typy zmiennych

---
+++ warszataty/javascript/js/typy_danych.js
@@ -0,0 +1,47 @@
+/*typ liczbowy - mozna wykonywac dzialania arytmetyczne*/
+var wyplata = 4500;
+var premia = 850;
+var suma = wyplata + premia;
+/*console.log(suma);
+console.log(typeof wyplata);*/
+
+/* typ łańcuchowy - string (znak plus łączny stringi, czyli dziła nieco inaczej niż przypadku typu liczbowego)*/
+
+var strWyplata = "4500\n";
+var strPremia = "\"850\"";
+var strSuma = strWyplata + strPremia;
+//console.log(strSuma);
+
+/* Typ logiczny przyjmuje jedna z dwoch wartosci true albo false */
+
+/*var czyToJestWiosna = true;*/
+var czyToJestWiosna = false;
+
+if(czyToJestWiosna){
+    console.log("Tak, to jest już wiosna");
+}else{
+    console.log("To, jeszcze nie jest wiosna");
+}
+
+/*console.log(typeof czyToJestWiosna);*/
+
+// typy spycjalne
+
+var zmiennaTylkoZadeklarowana;
+console.log(zmiennaTylkoZadeklarowana);
+
+var zmiennaNull = null;
+console.log(zmiennaNull);
+
+
+
+
+
+
+
+
+
+
+
+
+
