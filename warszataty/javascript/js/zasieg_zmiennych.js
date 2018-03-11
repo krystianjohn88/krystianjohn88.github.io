b32c659f82b0285665412fe993adf2864bdaaf12
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-11 09:45:15 +0100

    typyzmiennych

---
+++ warszataty/javascript/js/zasieg_zmiennych.js
@@ -0,0 +1,18 @@
+/*var zmiennaGlobalna = "Tak! Jestem zmienna globalną i jestem widoczna w funkcjach";
+
+function wyswietlZmienna() {
+    console.log(zmiennaGlobalna);
+    var zmiennaLokalna = "Tak! Zmienna lokalna to ja. Przestrzeń globalna mnie nie zobaczy";
+}
+
+wyswietlZmienna();
+console.log(zmiennaLokalna);*/
+
+var zmienna = "Zmienna globalna";
+
+function pokazZmienna() {
+    var zmienna = "Zmienna lokalna";
+    console.log(zmienna);
+}
+pokazZmienna();
+console.log(zmienna);
