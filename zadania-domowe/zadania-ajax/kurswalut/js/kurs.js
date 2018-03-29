9b9115355a0d23548ecd4ff653b920dc599ad6f2
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-03-29 11:11:11 +0200

    praca domowa

---
+++ zadania-domowe/zadania-ajax/kurswalut/js/kurs.js
@@ -0,0 +1,33 @@
+var aktualnaCenna = Number($("#kupno").html());
+
+function pobierzKurs(event){
+    event.preventDefault();
+   $.getJSON(  "https://blockchain.info/pl/ticker",   function (data) {      
+
+       var tablica = data.PLN;
+       var kursKupno = tablica.buy;
+       var kursSprzedaz = tablica.sell;
+       var kursKupno2 = kursKupno.toFixed(2);
+       var kursSprzedaz2 = kursSprzedaz.toFixed(2);
+       $("#kupno").html(kursKupno2);
+       $("#sprzedaz").html(kursSprzedaz2);
+       
+   
+    if(kursKupno2 > aktualnaCenna){
+       $("#kupnoStrzalka").html(" <i class='fas fa-arrow-up' style='color: green';></i>");
+        $("#sprzedazStrzalka").html(" <i class='fas fa-arrow-up' style='color: green';></i>");
+        
+       }else if(kursKupno2 < aktualnaCenna) {
+             $("#kupnoStrzalka").html(" <i class='fas fa-arrow-down' style='color: red';></i>");
+             $("#sprzedazStrzalka").html(" <i class='fas fa-arrow-down' style='color: red';></i>");
+           
+             }else{
+                 $("#kupnoStrzalka").html(" <strong>Bez Zmian</strong>");
+                 $("#sprzedazStrzalka").html(" <strong>Bez Zmian</strong>");
+             };
+    aktualnaCenna = Number($("#kupno").html());   
+   }) 
+    
+             
+   }; 
+
---
+++ zadania-domowe/zadania-ajax/pobierzDane/js/kurs.js
@@ -0,0 +1,24 @@
+function pobierzDane(event){
+    event.preventDefault();
+   $.getJSON(  "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",   function (data) { 
+       //Sprawdzam co znajduje sie w bazie danych ajax
+       console.log(data);
+       //Tworze zmienne z przypisanymi danymi z bazy ajax
+       var imie = data.imie;
+       var nazwisko = data.nazwisko;
+       var zawod = data.zawod;
+       var firma = data.firma;
+       
+       //Tworze nowy tag 'div' i nadaje mu id append powoduje dodanie na koncu elementu body "Niestety cały czas tworzy sie pusty div o id daneprogramisty
+       
+      /* $('body').append("<div id='dane-programisty'></div>");
+       $('#dane-programisty').html('Dane programisty<br> Imie: ' +imie+ '<br>Nazwisko: '+nazwisko+'<br>Zawód: '+zawod+'<br>Firma: '+firma);*/
+       
+       $('section').html("<div id='dane-programisty'></div>");
+       $('#dane-programisty').html('Dane programisty<br> Imie: ' +imie+ '<br>Nazwisko: '+nazwisko+'<br>Zawód: '+zawod+'<br>Firma: '+firma);
+
+   }) 
+    
+             
+   }; 
+
