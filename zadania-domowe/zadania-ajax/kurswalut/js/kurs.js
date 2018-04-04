6d30cf6a802db8715e179854d1d7192ef11e4e76
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-04-04 08:05:13 +0200

    Kurs walut risponsive

--- zadania-domowe/zadania-ajax/kurswalut/js/kurs.js
+++ zadania-domowe/zadania-ajax/kurswalut/js/kurs.js
@@ -6,28 +6,48 @@ function pobierzKurs(event){
     event.preventDefault();
    $.getJSON(  "https://blockchain.info/pl/ticker",   function (data) {  
     
+// Poczatek. Sprawdzam ktora waluta zostala wybrana i przypisuje do zmiennych kursKupno, kursSprzedaz wartosci z tablicy from ajax //       
+       var kursKupno;
+       var kursSprzedaz;
+       var sprawdzWybor = $('select').val();
+       var nazwyWalutTabela; 
+       function listaWalutWybor(tabela){
+        for(var w=0; w<Object.getOwnPropertyNames(tabela).length; w++){
+            var nazwaWaluty = Object.getOwnPropertyNames(tabela)[w]
+            if(sprawdzWybor == nazwaWaluty){
+                 kursKupno = Object.values(data)[w].buy;
+                 kursSprzedaz = Object.values(data)[w].sell;
+                 podpisWaluty = "BTC/"+nazwaWaluty;
+                 w = Object.getOwnPropertyNames(tabela).length;
+                }
+            }
+           }
+       listaWalutWybor(data);
+       
+// Poczatek. Sprawdzam ktora waluta zostala wybrana i przypisuje do zmiennych kursKupno, kursSprzedaz wartosci z tablicy from ajax //       
+       
       //------------Waluta wybor poczatek----------------
        console.log(Object.getOwnPropertyNames(data)[0]);
-       var sprawdzWybor = $('select').val();
+//       var sprawdzWybor = $('select').val();
        var podpisWaluty;
       
-       if(sprawdzWybor === 'PLN'){
-           tablica = data.PLN;
-           podpisWaluty = "BTC/PLN";
-       }else if(sprawdzWybor === 'EUR'){
-           tablica = data.EUR;
-           podpisWaluty = "BTC/EUR";
-       }else{
-           tablica = data.USD;
-           podpisWaluty =  "BTC/USD";
-       }
-       
+//       if(sprawdzWybor === 'PLN'){
+//           tablica = data.PLN;
+//           podpisWaluty = "BTC/PLN";
+//       }else if(sprawdzWybor === 'EUR'){
+//           tablica = data.EUR;
+//           podpisWaluty = "BTC/EUR";
+//       }else{
+//           tablica = data.USD;
+//           podpisWaluty =  "BTC/USD";
+//       }
+//       
        var jakaWal = $("#walutakupno").html()
        //------------Waluta wybor koniec----------------
 
        var tablica;
-       var kursKupno = tablica.buy;
-       var kursSprzedaz = tablica.sell;
+//       var kursKupno = tablica.buy;
+//       var kursSprzedaz = tablica.sell;
        var kursKupno2 = kursKupno.toFixed(2);
        var kursSprzedaz2 = kursSprzedaz.toFixed(2);
        $("#kupno").html(kursKupno2);
@@ -92,55 +112,52 @@ function pobierzKurs(event){
 
  //------------------------- Waluta poczatek ------------------------
        
-       var walutaPLN = 'PLN';
-       var walutaUSD = 'USD';
-       var walutaEUR = 'EUR';
-       var walutaWybrana;
-       var tablicaWalut = [walutaPLN,walutaUSD,walutaEUR];
-       
-       function wyborWaluty(tablica){
-           for(var i=0; i<tablica.length; i++){
-               walutaWybrana = tablica[i];
-               $('#jakaWaluta').append('<option  value='+walutaWybrana+'>'+walutaWybrana+'</option>');
-               
-           }
-           
-       };
+//       var walutaPLN = 'PLN';
+//       var walutaUSD = 'USD';
+//       var walutaEUR = 'EUR';
+//       var walutaWybrana;
+//       var tablicaWalut = [walutaPLN,walutaUSD,walutaEUR];
+//       
+//       function wyborWaluty(tablica){
+//           for(var i=0; i<tablica.length; i++){
+//               walutaWybrana = tablica[i];
+//               $('#jakaWaluta').append('<option  value='+walutaWybrana+'>'+walutaWybrana+'</option>');
+//               
+//           }
+//           
+//       };
        
 //       wyborWaluty(tablicaWalut);
       
        
        //------------------------Waluta koniec ---------------------------
 
-    
+
+//Dodałem po wczytaniu strony automatycznie wykonuje sie funkcja "wsztstkieWaluty" ktora tworzy liste wyboru walut
 document.addEventListener("DOMContentLoaded", function(event) {
     $.getJSON(  "https://blockchain.info/pl/ticker",   function (data) { 
     
-    //-------------------Waluty z bazy danych ajax tworza liste---------------
+    //-------------------Poczatek >>>>>  Waluty z bazy danych ajax tworza liste---------------
        
       
         var objectSymbols = Object.values(data)[1].sell;
-        console.log(objectSymbols);
         
-          var walWyb;
-    var tablicaWalut = Object.getOwnPropertyNames(data);
-        console.log(tablicaWalut);
-    function wszystkieWaluty(tablica){
-        for(n=0; n<tablica.length; n++){
-            walWyb = tablica[n];
-            $('#jakaWaluta').append('<option  value='+walWyb+'>'+walWyb+'</option>');
-             var proba = walWyb;
-           
-            
-//            console.log(proba);
-    }
-    }
+        var walWyb;
+        var tablicaWalut = Object.getOwnPropertyNames(data);
+            function wszystkieWaluty(tablica){
+                for(n=0; n<tablica.length; n++){
+                    walWyb = tablica[n];
+                    $('#jakaWaluta').append('<option  value='+walWyb+'>'+walWyb+'</option>');
+                     var proba = walWyb;
+                }
+            }
     wszystkieWaluty(tablicaWalut);
-    //--------Koniec funkcji Pobierz kurs ----------------   
+    //--------Koniec >>>>>>> Waluty z bazy danych ajax tworza liste ----------------   
     
     });
-    console.log("DOM został wczytany");
-    console.log("Tutaj dopiero wyłapujemy elementy");
 
 });
+
+
+
 
