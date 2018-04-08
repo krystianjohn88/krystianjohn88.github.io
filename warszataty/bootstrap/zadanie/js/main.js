e41fec91d7507f2ef4423503df259544a8a7a26a
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2018-04-08 16:46:29 +0200

    portfolio cdn

--- warszataty/bootstrap/zadanie/js/main.js
+++ warszataty/bootstrap/zadanie/js/main.js
@@ -3,5 +3,25 @@ window.onscroll = function zmianaTlaNawigacji(){
         $("#nawigacja").css("background-color", "rgba(59, 59, 59, 0.00)");
     }else{
     $("#nawigacja").css("background-color", "rgba(59, 59, 59, 0.69)");
+    $("#nawigacja").css("transition-duration", "1s");
     }
 }
+
+function przeniesienieDoServices(event){
+    event.preventDefault();
+//    console.log('dzial');
+//    var wysokosc = $('#services').offset().top;
+//    window.scrollTo(0,wysokosc);
+//    console.log(wysokosc);
+    $('html, body').animate(
+        {scrollTop: $('#services').offset().top }, 600
+   );
+   
+}
+
+$("a[href^='#']").on('click', function(event){
+      event.preventDefault();
+    $('html, body').animate(
+        {scrollTop: $(this.hash).offset().top }, 600
+    )
+   })
